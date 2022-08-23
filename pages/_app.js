e51c03c0970f09f765735ba35002bd3f14509734
Stephen Zweibel <oneperstephen@gmail.com>
2022-08-23 12:33:15 -0400

    bulma test

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <title>{yaml.event}</title>
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css" integrity="sha512-HqxHUkJM0SYcbvxUw5P60SzdOTy/QVwA1JJrvaXJv4q7lmbDZCmZaqz01UPOaQveoxfYRv1tHozWGPMcuTBuvQ==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
         {process.env.NEXT_PUBLIC_GITHUB_ACTIONS == false && <><link rel="icon" href={base + "/images/favicon.ico"} sizes="32x32"></link>
           <link rel="icon" href={base + "/images/favicon.ico"} sizes="192x192"></link>
           <link rel="apple-touch-icon" href={base + "/images/favicon.ico"}></link></>
